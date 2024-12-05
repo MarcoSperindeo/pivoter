8b52becefa156e09b0a7766a7b0bebbc59f68c94
marco.sperindeo <marco.sperindeo@satispay.com>
2024-12-06 00:02:45 +0100

    v1.00.08 - added comments to Pivoter class

--- src/main/java/org/pivoter/Pivoter.java
+++ src/main/java/org/pivoter/Pivoter.java
@@ -4,6 +4,7 @@ import org.pivoter.model.PivotTree;
 import org.pivoter.model.Row;
 import org.pivoter.model.PivotTreeNode;
 
+import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
@@ -19,17 +20,26 @@ public class Pivoter {
         this.pivotOrder = Comparator.naturalOrder();
     }
 
+    // add Javadoc
     // make it generic
     public PivotTreeNode pivot(List<Map<String, String>> data,
                                Comparator<String> pivotOrder) {
         // validate data rowᵢ = (L₁ᵢ, L₂ᵢ, L₃ᵢ, ..., Lₙᵢ, Vᵢ)
+            // maximum labels
+            // maximum rows
+            // labels should be string
+            // each row should have same no. of labels
+            // each row last element should be a double
         // build data rows sorting labels by pivot order
         // build pivot tree
         return null;
     }
 
+    // add Javadoc
     // make it generic
-    public Double query(PivotTree pivotTree, List<String> queryLabels) {
+    public Double query(PivotTree pivotTree,
+                        List<String> queryLabels,
+                        Function<Collection<Double>, Double> pivotFunction) {
         // sort query labels by pivot order
         // query tree by labels
         return null;
